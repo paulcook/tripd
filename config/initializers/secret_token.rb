3bc21cafd30811cdf7d70157deb6cd35c3dafcff
Paul Cook <paul.cook@alternateversion.com>
2013-01-18 14:39:42 -0500

    removed secret from project

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Tripd::Application.config.secret_token = '49ad07c289b031a929f404368e114dd8d015621a6e9aaf874ca67d628f2b107bea46ba120b587e922c27a43615ca45e801cc206b4a48fdb53ccc708d488df94d'
+Tripd::Application.config.secret_token = ENV["SECRET_TOKEN"]
